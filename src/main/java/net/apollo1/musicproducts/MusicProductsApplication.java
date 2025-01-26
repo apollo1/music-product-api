f5673a5834beed0bafa7321d398bf05d2182833f
Jamie Gladden <jamie.gladden@apollo-1.net>
2025-01-26 23:34:18 +0000

    Add Skeleton Spring Boot app

---
+++ src/main/java/net/apollo1/musicproducts/MusicProductsApplication.java
@@ -0,0 +1,13 @@
+package net.apollo1.musicproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicProductsApplication.class, args);
+	}
+
+}
