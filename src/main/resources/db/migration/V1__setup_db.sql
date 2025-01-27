2c3768abd590f337f18ad644b55cd07f5a29a7ad
Jamie Gladden <jamie.gladden@apollo-1.net>
2025-01-27 10:36:03 +0000

    Add flyway migration scripts for DB creation and initial seed data

---
+++ src/main/resources/db/migration/V1__setup_db.sql
@@ -0,0 +1,40 @@
+CREATE TABLE store
+(
+    id   UUID NOT NULL,
+    name text NOT NULL UNIQUE,
+    CONSTRAINT store_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE product_group
+(
+    id           UUID NOT NULL,
+    title        text NOT NULL,
+    release_date DATE NOT NULL,
+    CONSTRAINT product_group_pkey PRIMARY KEY (id)
+);
+
+CREATE TYPE distribution_type AS ENUM ('Physical', 'Digital');
+CREATE TYPE format_type AS ENUM ('Vinyl', 'CD', 'Cassette', 'MP3', 'WAV', 'WAV24');
+
+CREATE TABLE product
+(
+    id               UUID NOT NULL,
+    title            text NOT NULL,
+    distribution     distribution_type,
+    format           format_type,
+    price_gbp        NUMERIC,
+    price_usd        NUMERIC,
+    price_eur        NUMERIC,
+    release_date     DATE NOT NULL,
+    store_id         UUID NOT NULL,
+    product_group_id UUID NOT NULL,
+    CONSTRAINT product_pkey PRIMARY KEY (id),
+    CONSTRAINT store_id_fkey FOREIGN KEY (store_id) REFERENCES store (id),
+    CONSTRAINT product_group_id_fkey FOREIGN KEY (product_group_id) REFERENCES product_group (id)
+);
+
+CREATE INDEX store_name_idx ON store USING btree (name);
+CREATE INDEX product_group_title_idx ON product_group USING btree (title);
+CREATE INDEX product_group_release_date_idx ON product_group USING btree (release_date);
+CREATE INDEX product_title_idx ON product USING btree (title);
+CREATE INDEX product_release_date_idx ON product USING btree (release_date);
---
+++ src/main/resources/db/migration/V2__seed_data.sql
@@ -0,0 +1,20 @@
+-- Insert Stores
+insert into store (id, name)
+values ('023e4795-5f59-4650-bd86-d7990803759c', 'Sister Ray'),
+       ('fe22ec46-383b-46a0-a7f0-470bc5977f57', 'Rough Trade');
+
+-- Insert Product Groups
+insert into product_group (id, title, release_date)
+values ('e38abc59-47ef-4610-96f1-fbdc0b2fd9b0', 'Music', '2021-08-07'),
+       ('bfb4c101-ac4d-4172-8c85-14228f85acd1', 'Apparel', '2022-01-01'),
+       ('ac7297d9-a74d-486e-9bca-bd714f33dc23', 'Merch', '2023-12-31');
+
+-- Insert Products
+insert into product (id, title, distribution, format, price_gbp, price_usd, price_eur, release_date, store_id,
+                     product_group_id)
+values ('15ac6b6c-e6e8-423c-b5a8-bf7d44f43e77', 'Chelsea Wolfe CD', 'Physical', 'CD', 19.99, 17.99, 18.99, '2024-03-15',
+        '023e4795-5f59-4650-bd86-d7990803759c', 'e38abc59-47ef-4610-96f1-fbdc0b2fd9b0'),
+       ('34ae6825-59d3-4e14-b7f5-5e648464011c', 'Goat Girl MP3', 'Digital', 'MP3', 14.99, 18.99, 16.99, '2023-08-01',
+        'fe22ec46-383b-46a0-a7f0-470bc5977f57', 'e38abc59-47ef-4610-96f1-fbdc0b2fd9b0'),
+       ('c540296b-df4d-49fe-b856-8355764b27d8', 'Fraulein Cassette', 'Physical', 'Cassette', 9.99, 8.99, 7.99,
+        '2023-06-20', 'fe22ec46-383b-46a0-a7f0-470bc5977f57', 'ac7297d9-a74d-486e-9bca-bd714f33dc23');
